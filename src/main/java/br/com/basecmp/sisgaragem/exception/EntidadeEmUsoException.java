d9d01761d38f98bdddae17cf08b03d9a05354cc3
valberjunior <dev.junior.medeiros@gmail.com>
2019-11-10 16:23:49 -0300

    CRUD de viaturas implementado

---
+++ src/main/java/br/com/basecmp/sisgaragem/exception/EntidadeEmUsoException.java
@@ -0,0 +1,8 @@
+package br.com.basecmp.sisgaragem.exception;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+    public EntidadeEmUsoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/basecmp/sisgaragem/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,8 @@
+package br.com.basecmp.sisgaragem.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
