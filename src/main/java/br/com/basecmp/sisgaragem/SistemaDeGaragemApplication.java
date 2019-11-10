152cb62eded5d81b96f501893acc510e35873126
valberjunior <dev.junior.medeiros@gmail.com>
2019-11-10 16:19:38 -0300

    Initial commit

---
+++ src/main/java/br/com/basecmp/sisgaragem/SistemaDeGaragemApplication.java
@@ -0,0 +1,13 @@
+package br.com.basecmp.sisgaragem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGaragemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeGaragemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/basecmp/sisgaragem/SistemaDeGaragemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.basecmp.sisgaragem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGaragemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
